19f7ac9cc9bdc1ac76a3efa8f497885d606d611a
Villa Contessa <41233512+villa-contessa@users.noreply.github.com>
2023-12-02 15:34:39 +0100

    Updates für Fancybox
    CSS für Fancybox aktualisiert + roompriceconverter.js aktualisiert

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-
    var lightboxLinks = document.querySelectorAll(".lightbox-link");
-  
-  // Für die Nutzung von Fancybox, um alle Fotos einer Zimmerkategorie innerhalb einer Fancybox verfügbar zu machen (Fancybox Gallery)
+
+   // Für die Nutzung von Fancybox, um alle Fotos einer Zimmerkategorie innerhalb einer Fancybox verfügbar zu machen (Fancybox Gallery)
    lightboxLinks.forEach(function (link) {
       // Die Zimmerkategorie wird aus dem Attribut des Collection List Items ausgelesen:
       var roomCategoryElement = link.closest("[data-roomcategory]");
@@ -14,58 +13,48 @@ document.addEventListener("DOMContentLoaded", function () {
       link.setAttribute("href", imgSrc);
       link.setAttribute("data-thumb", imgSrc);
       link.setAttribute("data-src", imgSrc);
-   });  
+   });
 
-    // Konfiguration von Fancybox:
-    Fancybox.bind(".lightbox-link", {
-        defaultDisplay: "flex",
-        backdropClick: "close",
-        dragToClose: false,
-        Toolbar: {
-           display: {
-              left: [],
-              middle: [],
-              right: ["close"],
-           },
-        },
-        contentClick: false,
-        closeButton: false,
-        idle: false,
-        Thumbs: {
-           type: "classic",
-        },
-        wheel: false,
-        on: {
-           load: function (fancybox, slide) {
-              document.body.classList.add("hidden");
-           },
-           destroy: function (fancybox, slide) {
-              document.body.classList.remove("hidden");
-           },
-        },
-     });
-  
-     // Beim Klick auf den Fotogalerie-Link und beim Klick auf das Hauptfoto der Zimmerkategorie wird die Lightbox / Fancybox ebenfalls geöffnet:
-     var galleryLinks = document.querySelectorAll(".photo-gallery-link, .zimmer-photos_fancybox-start");
-  
-     galleryLinks.forEach(function (link) {
-        link.addEventListener("click", function (e) {
-           e.preventDefault();
-           var zimmerPhotos = this.closest(".zimmer-photos");
-           var nearestLightboxLink = zimmerPhotos.querySelector(".lightbox-link");
-           if (nearestLightboxLink) {
-              nearestLightboxLink.click();
-           }
-        });
-     });
+   // Konfiguration von Fancybox:
+   Fancybox.bind(".lightbox-link", {
+      defaultDisplay: "flex",
+      backdropClick: "close",
+      dragToClose: false,
+      Toolbar: {
+         display: {
+            left: [],
+            middle: [],
+            right: ["close"],
+         },
+      },
+      contentClick: false,
+      closeButton: false,
+      idle: false,
+      Thumbs: {
+         type: "classic",
+      },
+      wheel: false,
+      on: {
+         load: function (fancybox, slide) {
+            document.body.classList.add("hidden");
+         },
+         destroy: function (fancybox, slide) {
+            document.body.classList.remove("hidden");
+         },
+      },
+   });
 
-// Beim Klick auf den Gallery-Link und beim Klick auf das Hauptfoto der Zimmerkategorie wird die Lightbox / Fancybox geöffnet
-$(".photo-gallery-link")
-.add(".zimmer-photos_fancybox-start")
-.on("click", function (e) {
-   e.preventDefault();
-   var nearest_lightbox_link = $(this).closest(".zimmer-photos").find(".lightbox-link:first");
-   nearest_lightbox_link[0].click();
-});
+   // Beim Klick auf einen Fotogalerie-Link wird die Lightbox / Fancybox ebenfalls geöffnet:
+   var galleryLinks = document.querySelectorAll(".photo-gallery-link");
 
+   galleryLinks.forEach(function (link) {
+      link.addEventListener("click", function (e) {
+         e.preventDefault();
+         var zimmerPhotos = this.closest(".zimmer-photos");
+         var nearestLightboxLink = zimmerPhotos.querySelector(".lightbox-link");
+         if (nearestLightboxLink) {
+            nearestLightboxLink.click();
+         }
+      });
+   });
 });
--- js/roompriceconverter.js
+++ js/roompriceconverter.js
@@ -1,17 +1,25 @@
 // Function, mit der bei den Preisen, die aus dem Webflow CMS importiert werden, ein Punkt als Tausender-Trennzeichen eingefügt wird
-function RoomPriceConverter(selector) {
-    let number = $(selector).text();
-    let decimals = 0; // Anzahl der gewünschten Nachkommastellen
-    let decpoint = ","; // Trennzeichen für Nachkommastellen
-    let thousand = "."; // Trennzeichen für Tausenderzahlen
- 
-    let n = Math.abs(number).toFixed(decimals).split(".");
-    n[0] = n[0]
-       .split("")
-       .reverse()
-       .map((c, i, a) => (i > 0 && i < a.length && i % 3 == 0 ? c + thousand : c))
-       .reverse()
-       .join("");
-    let final = (Math.sign(number) < 0 ? "-" : "") + n.join(decpoint);
-    $(selector).text(final);
- }
+document.addEventListener("DOMContentLoaded", function () {
+   function RoomPriceConverter(selector) {
+      let number = $(selector).text();
+      let decimals = 0; // Anzahl der gewünschten Nachkommastellen
+      let decpoint = ","; // Trennzeichen für Nachkommastellen
+      let thousand = "."; // Trennzeichen für Tausenderzahlen
+
+      let n = Math.abs(number).toFixed(decimals).split(".");
+      n[0] = n[0]
+         .split("")
+         .reverse()
+         .map((c, i, a) => (i > 0 && i < a.length && i % 3 == 0 ? c + thousand : c))
+         .reverse()
+         .join("");
+      let final = (Math.sign(number) < 0 ? "-" : "") + n.join(decpoint);
+      $(selector).text(final);
+   }
+
+   // Bei den Zimmerpreisen wird ein Punkt als Tausender-Trennzeichen eingefügt (unsere Custom Function "RoomPriceConverter()" haben wir in unserer "script.js" hinterlegt):
+   let roomPriceElements = document.querySelectorAll(".room-price");
+   roomPriceElements.forEach(function (element) {
+      RoomPriceConverter(element);
+   });
+});
