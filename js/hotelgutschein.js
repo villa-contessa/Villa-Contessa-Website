58ccc8273b4ec59c1641564063edc7b88c91959b
Villa Contessa <41233512+villa-contessa@users.noreply.github.com>
2023-12-13 20:00:21 +0100

    Update style.css

--- js/hotelgutschein.js
+++ js/hotelgutschein.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
    var selectField_Category = document.getElementById("option-set-df94d7e3772905b4d2d31f8d798ed2f8");
    var selectField_Nights = document.getElementById("option-set-c085c45f0dc9fd04675a15f2315d12a5");
    var selectField_Saison = document.getElementById("option-set-377b8b8db2bd6e32205ce1863c61919c");
-   var InfosWinterSaison = $('[shop-selected-saison="winter"]');
-   var InfosSommerSaison = $('[shop-selected-saison="sommer"]');
    var AnzahlNaechteElement = document.querySelector('[villa-custom-attribute="gutschein-number-of-nights"]');
    var preisAnzeige = $('[villa-custom-attribute="gutschein-preisanzeige"]');
    var isAnimating = false; // Variable zum Überwachen des Animationsstatus
@@ -15,13 +13,34 @@ document.addEventListener("DOMContentLoaded", function () {
    var validationMessage_Category = $('[villa-custom-attribute="gutschein-validation-message-zimmerkategorie"]');
    var validationMessage_Nights = $('[villa-custom-attribute="gutschein-validation-message-nights"]');
    var validationMessage_Saison = $('[villa-custom-attribute="gutschein-validation-message-saison"]');
+   var selectField_RoomCategory_InfosSection = document.querySelector('[data-vc-shop-element="select-roomcategory-section-zimmerinfos"]');
    var submitButton = document.querySelector('input[type="submit"]');
 
    selectField_Category.addEventListener("change", function () {
       // Eventuell eingeblendete Validation-Message wird wieder ausgeblendet, sobald eine Zimmerkategorie  ausgewählt wurde:
       validationMessage_Category.slideUp(430);
-      // Infos für die gewählte Zimmerkategorie werden eingeblendet:
       var selectedCategory = selectField_Category.options[selectField_Category.selectedIndex].innerText;
+      // Alle Zimmerinfos der zuvor gewählten Zimmerkategorie werden zunächst ausgeblendet:
+      InfosZimmerkategorie.forEach(function (item) {
+         item.style.display = "none";
+      });
+      // Jetzt werden die Infos für die gewählte Zimmerkategorie eingeblendet:
+      var categoryToShow = document.querySelectorAll(`[data-roomcategory='${selectedCategory}']`);
+      categoryToShow.forEach(function (item) {
+         item.style.display = "block";
+      });
+
+      // Überträgt die ausgewählte Zimmerkategorie auch im Select-Field in der Section "Informationen Zimmerkategorie":
+      if (selectField_RoomCategory_InfosSection) {
+         selectField_RoomCategory_InfosSection.value = selectedCategory;
+         selectField_RoomCategory_InfosSection.dispatchEvent(new Event("change"));
+      }
+   });
+
+   // Wird über das Select Field in der Section "Informationen Zimmerkategorie" eine Zimmerkategorie ausgewählt,
+   // dann werden die Infos für diese Zimmerkategorie eingeblendet:
+   selectField_RoomCategory_InfosSection.addEventListener("change", function () {
+      var selectedCategory = selectField_RoomCategory_InfosSection.options[selectField_RoomCategory_InfosSection.selectedIndex].innerText;
       // Alle Zimmerinfos der zuvor gewählten Zimmerkategorie werden zunächst ausgeblendet:
       InfosZimmerkategorie.forEach(function (item) {
          item.style.display = "none";
@@ -79,24 +98,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
          // Eventuell eingeblendete Validation-Message wird wieder ausgeblendet, sobald eine Saison ausgewählt wurde:
          validationMessage_Saison.slideUp(430);
-
-         // Infos zu den Saison-Zeiten werden eingeblendet:
-         var selectedSeason = this.innerText;
-         var fadeInElement = selectedSeason === "Winter" ? InfosWinterSaison : InfosSommerSaison;
-         var fadeOutElement = selectedSeason === "Winter" ? InfosSommerSaison : InfosWinterSaison;
-
-         isAnimating = true; // Setze den Animationsstatus auf "läuft"
-
-         fadeOutElement.fadeOut(350, function () {
-            fadeInElement.fadeIn(350, function () {
-               isAnimating = false; // Setze den Animationsstatus zurück, wenn die Animation beendet ist
-            });
-         });
       });
    });
-   // Preis für den Hotelgutschein wird erst eingeblendet und Add-to-Cart-Button wird erst freigeschaltet, wenn alle erforderlichen Optionen ausgewählt wurden (Zimmerkategorie + Anzahl Nächte + Saison)
+
+   // Der Preis für den Gutschein wird erst angezeit, wenn alle erforderlichen Optionen ausgewählt wurden (Zimmerkategorie + Anzahl Nächte + Saison)
+   [selectField_Category, selectField_Nights, selectField_Saison].forEach((field) => {
+      field.addEventListener("change", function () {
+         if (selectField_Category.value && selectField_Nights.value && selectField_Saison.value) {
+            preisAnzeige.fadeIn(350);
+         } else {
+            preisAnzeige.fadeOut(350);
+         }
+      });
+   });
+
+   // Der Add-to-Cart-Button wird erst freigeschaltet, wenn alle erforderlichen Optionen ausgewählt wurden (Zimmerkategorie + Anzahl Nächte + Saison)
    // Wenn nicht alle erforderlichen Select Optionen gewählt wurden, dann wird die entsprechende Validierungsnachricht angezeigt
-   function checkAllSelects() {
+   submitButton.addEventListener("click", function (e) {
       let allSelected = true;
 
       // Zimmerkategorie
@@ -123,28 +141,43 @@ document.addEventListener("DOMContentLoaded", function () {
          validationMessage_Saison.hide();
       }
 
-      return allSelected;
-   }
-
-   // Event-Listener für den Klick auf den Warenkorb-Button
-   submitButton.addEventListener("click", function (e) {
-      if (!checkAllSelects()) {
+      if (!allSelected) {
          e.preventDefault(); // Verhindern, dass das Formular abgesendet wird
          preisAnzeige.fadeOut(350);
       } else {
          preisAnzeige.fadeIn(350);
       }
    });
+
+   // Der gewünschte Placeholder-Text für das Zimmerkategorie-Dropdown wird gesetzt:
+   var selectElement_Roomcategory_AddToCart = document.getElementById("option-set-df94d7e3772905b4d2d31f8d798ed2f8");
+   var selectElement_Roomcategory_RoomInfos = document.getElementById("select-field_roomcategory_roominfos");
+   var custom_placeholder_Roomcategory_AddToCart = $('[data-vc-shop-element="placeholder_roomcategory_addtocart"]');
+
+   function updatePlaceholderText() {
+      var screenWidth = window.innerWidth;
+      var Placeholder_Roomcategory_AddToCart = selectElement_Roomcategory_AddToCart.options[0];
+      var Placeholder_Roomcategory_RoomInfos = selectElement_Roomcategory_RoomInfos.options[0];
+
+      if (screenWidth >= 500) {
+         // Placeholder-Text für größere Screens:
+         Placeholder_Roomcategory_AddToCart.textContent = "Bitte wählen Sie eine Zimmerkategorie!";
+         Placeholder_Roomcategory_RoomInfos.textContent = "Bitte wählen Sie eine Zimmerkategorie!";
+      } else {
+         // Placeholder-Text für kleine Screens:
+         Placeholder_Roomcategory_AddToCart.textContent = "Bitte wählen Sie!";
+         Placeholder_Roomcategory_RoomInfos.textContent = "Bitte wählen Sie!";
+      }
+   }
+
+   // Setzen des Placeholder-Texts nach dem Laden der Seite:
+   updatePlaceholderText();
+   $(custom_placeholder_Roomcategory_AddToCart).css("color", "#777061").fadeIn(800);
 });
 
-// Der gewünschte Placeholder-Text für das Zimmerkategorie-Dropdown wird gesetzt:
-
 $(window).on("load", function () {
-   // Ändern des Placeholder-Texts nach dem Laden der Seite
-   $(".placeholder-text").text("Bitte wählen Sie!").css("color", "#777061");
-
    // Event-Handler für das Klicken auf ein Option-Element
-   $(".dropdown-link").click(function () {
+   $('[data-vc-shop-element="zimmerkategorie-dropdown-link"]').click(function () {
       var selectedText = $(this).text();
       $(".placeholder-text").text(selectedText);
    });
